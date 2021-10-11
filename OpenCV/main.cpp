baf5de19a866afd411852d62b20820111b2a7365
Umut Güngör <Umsko@outlook.com>
2021-10-11 14:34:38 +0200

    Forest and Window created

--- OpenCV/main.cpp
+++ OpenCV/main.cpp
@@ -1,5 +1,9 @@
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
 int main() {
-	cv::Mat = 
+	cv::Mat frame;
+	frame = cv::imread("baum.jpg", cv::IMREAD_COLOR);
+	cv::imshow("frame", frame);
+	cv::waitKey(0);
 }
